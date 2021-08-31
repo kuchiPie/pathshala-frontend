949c0cb874f0ed11936f2aea04ab236f37f8eccc
kuchiPie <shreyasbulbule007@gmail.com>
2021-08-31 16:52:49 +0530

    minor changes in navabar and footer

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -144,13 +144,10 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
-      <div id='footer'>
+      <div id='footer' >
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
+            &copy; 2021 The Wolf Pack
           </p>
         </div>
       </div>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -38,11 +38,6 @@ export const Navigation = (props) => {
               </a>
             </li>
             <li>
-              <a href='#testimonials' className='page-scroll'>
-                Testimonials
-              </a>
-            </li>
-            <li>
               <a href='#team' className='page-scroll'>
                 Team
               </a>
